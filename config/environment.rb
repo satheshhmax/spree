8dc6721d3dfa4da2cc4eaa79cb963ec58a7d1255
Roman Smirnov <POMAHC@gmail.com>
2009-08-15 00:47:21 +0400

    Update environmet.rb

--- config/environment.rb
+++ config/environment.rb
@@ -1,12 +1,12 @@
 # Be sure to restart your web server when you modify this file.
 
-# Uncomment below to force Rails into production mode when 
+# Uncomment below to force Rails into production mode when
 # you don't control web/app server and can't set it the proper way
 # ENV['RAILS_ENV'] ||= 'production'
 
 # Specifies gem version of Rails to use when vendor/rails is not present
 SPREE_GEM_VERSION = '0.8.99' unless defined? SPREE_GEM_VERSION
-          
+
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
@@ -20,7 +20,7 @@ Spree::Initializer.run do |config|
   # you must remove the Active Record framework.
   # config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
 
-  # Specify gems that this application depends on. 
+  # Specify gems that this application depends on.
   # They can then be installed with "rake gems:install" on new installations.
   # config.gem "bj"
   # config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
@@ -33,22 +33,20 @@ Spree::Initializer.run do |config|
   config.gem 'activerecord-tableless', :lib => 'tableless', :version => '>=0.1.0'
   config.gem 'haml-edge', :lib=> 'haml', :version => "2.1.37"
   config.gem 'chriseppstein-compass', :lib => 'compass', :source => "http://gems.github.com", :version => '0.6.15'
-  config.gem 'calendar_date_select', :version => '1.15'    
+  config.gem 'calendar_date_select', :version => '1.15'
   config.gem 'rsl-stringex', :lib => 'stringex', :source => "http://gems.github.com"
 
   config.gem 'searchlogic', :version => '2.1.13'
-  config.gem 'mislav-will_paginate', :version => '~> 2.3.11', :lib => 'will_paginate', 
+  config.gem 'mislav-will_paginate', :version => '~> 2.3.11', :lib => 'will_paginate',
         :source => 'http://gems.github.com'
 
-  config.gem "newrelic_rpm"
-
-  # Only load the plugins named here, in the order given. By default, all plugins 
+  # Only load the plugins named here, in the order given. By default, all plugins
   # in vendor/plugins are loaded in alphabetical order.
   # :all can be used as a placeholder for all plugins not explicitly named
   # config.plugins = [ :exception_notification, :ssl_requirement, :all ]
-  config.plugins = [:all, :resource_controller, :extension_patches ] 
-  
-  config.extensions = [:static_content, :russian_adaptation, :all, :ortomed] 
+  config.plugins = [:all, :resource_controller, :extension_patches ]
+
+  config.extensions = [:localization, :static_content, :russian_adaptation, :all, :ortomed]
   # Add additional load paths for your own custom dirs
   # config.load_paths += %W( #{RAILS_ROOT}/extras )
 
@@ -58,7 +56,7 @@ Spree::Initializer.run do |config|
 
   # Make Time.zone default to the specified zone, and make Active Record store time values
   # in the database in UTC, and return them converted to the specified local zone.
-  # Run "rake -D time" for a list of tasks for finding time zone names. 
+  # Run "rake -D time" for a list of tasks for finding time zone names.
   config.time_zone = "Eastern Time (US & Canada)"
 
   # Use SQL instead of Active Record's schema dumper when creating the test database.
@@ -67,19 +65,19 @@ Spree::Initializer.run do |config|
   config.active_record.schema_format = :sql
 
   # Activate observers that should always be running
-  # config.active_record.observers = :cacher, :garbage_collector  
-  
+  # config.active_record.observers = :cacher, :garbage_collector
+
   # The internationalization framework can be changed to have another default locale (standard is :en) or more load paths.
   # All files from config/locales/*.rb,yml are added automatically.
   #config.i18n.load_path << Dir[File.join(RAILS_ROOT, 'my', 'locales', '*.{rb,yml}')]
-  config.i18n.default_locale = :'en-US'    
-    
+  config.i18n.default_locale = :'en-US'
+
 end
 
 Time::DATE_FORMATS[:date_time24] = "%Y-%m-%d %H:%M"
 Time::DATE_FORMATS[:short_date] = "%Y-%m-%d"
 
-# Add new inflection rules using the following format 
+# Add new inflection rules using the following format
 # (all these examples are active by default):
 # Inflector.inflections do |inflect|
 #   inflect.plural /^(ox)$/i, '\1en'
@@ -93,3 +91,4 @@ Time::DATE_FORMATS[:short_date] = "%Y-%m-%d"
 # Mime::Type.register "application/x-mobile", :mobile
 
 # Include your application configuration below
+
