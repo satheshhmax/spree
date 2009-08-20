a99bafcd4a180fa461176fdf7eea9ab6e8b18cb0
Roman Smirnov <POMAHC@gmail.com>
2009-08-20 18:17:45 +0400

    Add parse date params before_filter.

--- app/controllers/admin/base_controller.rb
+++ app/controllers/admin/base_controller.rb
@@ -6,8 +6,26 @@ class Admin::BaseController < Spree::BaseController
   before_filter :initialize_order_admin_tabs
   before_filter :initialize_extension_tabs
   before_filter :add_shipments_tab
+  before_filter :parse_date_params
 
 private
+  def parse_date_params
+    dates = [] 
+    if (params[object_name])       
+      params[object_name].each do |k, v|
+        if k =~ /\(\di\)$/
+          param_name = k[/^\w+/]
+          dates << param_name
+        end
+      end
+      if (dates.size > 0)
+        dates.uniq.each do |date|
+          params[object_name][date] = [params[object_name].delete("#{date}(2i)"), params[object_name].delete("#{date}(3i)"), params[object_name].delete("#{date}(1i)")].join('/')
+        end    
+      end
+    end
+  end
+
   def add_extension_admin_tab(tab_args)
     @extension_tabs << tab_args
   end
